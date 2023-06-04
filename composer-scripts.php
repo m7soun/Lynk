751c2be592399d122b1e6dd278f2ac11848c4637
MOHAMMAD ALI <mohammad.ali@lyveglobal.com>
2023-06-04 22:26:48 +0400

    noth

--- composer-scripts.php
+++ composer-scripts.php
@@ -2,8 +2,13 @@
 
 $scriptDir = __DIR__;
 $source = realpath($scriptDir . DIRECTORY_SEPARATOR . 'lynk');
-$destination = dirname(dirname($scriptDir)) . DIRECTORY_SEPARATOR . 'lyve-packages' . DIRECTORY_SEPARATOR . 'packages-tester' . DIRECTORY_SEPARATOR . 'linker';
+$destination = dirname(dirname($scriptDir)) . DIRECTORY_SEPARATOR . 'lyve-packages' . DIRECTORY_SEPARATOR . 'packages-tester' . DIRECTORY_SEPARATOR . 'lynk';
 
+echo 'Publishing the linker file to the root of your project...', PHP_EOL;
+echo 'Source: ', $source, PHP_EOL;
+echo 'Destination: ', $destination, PHP_EOL;
+echo PHP_EOL;
+echo 'If you want to use the linker, you will need to run this script again.', PHP_EOL;
 if (!file_exists($destination)) {
     copy($source, $destination);
     chmod($destination, 0755);
