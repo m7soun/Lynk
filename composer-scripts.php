aedc6cf355cc16ae579267e6c91c276017c79c35
MOHAMMAD ALI <mohammad.ali@lyveglobal.com>
2023-06-04 22:47:18 +0400

    noth

--- composer-scripts.php
+++ composer-scripts.php
@@ -1,8 +1,7 @@
 <?php
-
-$packageRootDir = realpath(__DIR__ . '/../..'); // Adjust the path to match the root directory of your package
-$source = $packageRootDir . DIRECTORY_SEPARATOR . 'lynk';
-$destination = dirname(dirname($packageRootDir)) . DIRECTORY_SEPARATOR . 'lynk';
+$scriptDir = __DIR__;
+$source = realpath($scriptDir . DIRECTORY_SEPARATOR . 'lynk');
+$destination = dirname(dirname(dirname($scriptDir))) . DIRECTORY_SEPARATOR . 'project' . DIRECTORY_SEPARATOR . 'lynk';
 
 echo 'Publishing the linker file to the root of your project...', PHP_EOL;
 echo 'Source: ', $source, PHP_EOL;
@@ -11,12 +10,9 @@ echo PHP_EOL;
 echo 'If you want to use the linker, you will need to run this script again.', PHP_EOL;
 
 if (!file_exists($destination)) {
-    if (copy($source, $destination)) {
-        chmod($destination, 0755);
-        echo 'The linker file has been published to the root of your project.', PHP_EOL, $destination;
-    } else {
-        echo 'Failed to copy the linker file.', PHP_EOL;
-    }
+    copy($source, $destination);
+    chmod($destination, 0755);
+    echo 'The linker file has been published to the root of your project.', PHP_EOL, $destination;
 } else {
     echo 'The linker file already exists in the root of your project.', PHP_EOL;
 }
