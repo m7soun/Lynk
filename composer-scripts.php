a1076bbb03beb5809190f2dbadf79120b3bc1ced
MOHAMMAD ALI <mohammad.ali@lyveglobal.com>
2023-06-04 22:36:14 +0400

    noth

--- composer-scripts.php
+++ composer-scripts.php
@@ -9,10 +9,3 @@ echo 'Source: ', $source, PHP_EOL;
 echo 'Destination: ', $destination, PHP_EOL;
 echo PHP_EOL;
 echo 'If you want to use the linker, you will need to run this script again.', PHP_EOL;
-if (!file_exists($destination)) {
-    copy($source, $destination);
-    chmod($destination, 0755);
-    echo 'The linker file has been published to the root of your project.', PHP_EOL, $destination;
-} else {
-    echo 'The linker file already exists in the root of your project.', PHP_EOL;
-}
